43fd025911fc6dfa94f3493e202151209a5fa9f2
hyune0129™ hyune0129™ <hyune0129@naver.com>
2023-06-21 18:31:55 +0900

    permission

---
+++ AIServer/IPPermission.py
@@ -0,0 +1,14 @@
+"""
+simple middlware to block IP addresses via settings 
+variable BLOCKED_IPS
+"""
+from django.conf import settings
+from django import http
+
+class SafeIPPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if ip := request.META.get('HTTP_X_FORWARDED_FOR'):
+            ip = ip.split(',')[-1]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip in settings.ALLOWED_HOSTS
